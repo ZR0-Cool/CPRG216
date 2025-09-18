9c3611235bc4ac2492b9e4a7943ef7cd93b8c47a
ZR0-cool <jacobt.calgary@gmail.com>
2025-09-18 15:41:07 -0600

    week 3 assignment done in class

---
+++ week3.py
@@ -0,0 +1,14 @@
+'''
+ this is our third class demo, we will be working on variables, operators, and stuff i guess
+'''
+# Review
+# assignment
+
+x = 5 # int
+y = 2.5 # float
+print("x is",x,"and is", type(x))
+print("y is",y,"and is", type(y))
+z = "this statement is false" # string
+print("z is",z,"and is", type(z))
+v = False # bool
+print("v is",v,"and is", type(v))
